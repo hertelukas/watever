0065d9be667664704facb4a8d0c47cd410d6a4cc
Lukas <lukas.hertel@tum.de>
2026-01-07 18:15:41 +0100

    test(exec): fibonacci calculation in C++

---
+++ test/execution/fib.cpp
@@ -0,0 +1,33 @@
+// RUN: /opt/wasi-sdk/bin/clang++ -S -emit-llvm --target=wasm32-wasip2 -fno-exceptions %s -o %t.ll 
+// RUN: watever %t.ll -o %t.o
+// RUN: /opt/wasi-sdk/bin/clang++ %t.o -o %t.wasm
+// RUN: wasmtime %t.wasm 21 | FileCheck %s
+
+#include <iostream>
+#include <string>
+
+struct Data {
+  int a;
+  int b;
+
+  Data(int a, int b) : a{a}, b{b} {}
+};
+
+static int fib(int n, Data d) {
+  if (n == 0) {
+    return d.a + d.b;
+  }
+  auto D = Data(d.b, d.a + d.b);
+  return fib(n - 1, D);
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2)
+    return 1;
+  int num = std::stoi(argv[1]);
+  auto D = Data(0, 1);
+  int res = fib(num - 2, D);
+  std::cout << "The " << num << "th fibonacci number is " << res << "\n";
+}
+
+// CHECK: The 21th fibonacci number is 10946
